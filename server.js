9378fa75951c7a170e06a5d88206acf99a9c500b
amasin76 <51626916+amasin76@users.noreply.github.com>
2021-01-27 19:31:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const fetch = require('node-fetch');
 const tutorialBot = require("./handler/ClientBuilder.js"); // We're gonna create this soon.
 const client = new tutorialBot();
 
+const fs = require("fs");
+const db = require("wio.db");
+const canvas = require('discord-canvas'),
+    welcomeCanvas = new canvas.Welcome(),
+    leaveCanvas = new canvas.Goodbye()
+const {
+    unicolor,
+    imageLink,
+    channel
+} = require('./config.json')
+
 require("./handler/module.js")(client);
 require("./handler/Event.js")(client);
 
@@ -11,7 +22,7 @@ client.on("warn", console.warn); // This will warn you via logs if there was som
 client.on("error", console.error); // This will send you an error message via logs if there was something missing with your coding.
 client.login(process.env.SECRET).catch(console.error); // This token will leads to the .env file. It's safe in there.
 
-//DM Reverse==============================================
+//DM Reverse ==============================================
 client.on("message", (message) => {
             if (message.channel.type === "dm") {
         if (message.author.id === client.user.id) return;
@@ -27,6 +38,55 @@ client.on("message", (message) => {
                 client.users.cache.get("484524591696576523").send(yumz)
             }
 });
+
+//Canvas Welcome & Leave ====================================
+client.on('guildMemberAdd', async member => {
+    let image = await welcomeCanvas
+        .setUsername(member.user.username)
+        .setDiscriminator(member.user.discriminator)
+        .setMemberCount(member.guild.memberCount)
+        .setGuildName(member.guild.name)
+        .setAvatar(member.user.displayAvatarURL({
+            format: 'png'
+        }))
+        .setColor("border", universalColor)
+        .setColor("username-box", universalColor)
+        .setColor("discriminator-box", universalColor)
+        .setColor("message-box", universalColor)
+        .setColor("title", universalColor)
+        .setColor("avatar", universalColor)
+        .setBackground(imageLink)
+        .toAttachment()
+
+
+    let attachment = new Discord.MessageAttachment(image.toBuffer(), "welcome-image.png");
+
+    member.guild.channels.cache.find(c => c.id === channel).send(attachment)
+})
+
+client.on('guildMemberRemove', async member => {
+    let image = await leaveCanvas
+        .setUsername(member.user.username)
+        .setDiscriminator(member.user.discriminator)
+        .setMemberCount(member.guild.memberCount)
+        .setGuildName(member.guild.name)
+        .setAvatar(member.user.displayAvatarURL({
+            format: 'png'
+        }))
+        .setColor("border", universalColor)
+        .setColor("username-box", universalColor)
+        .setColor("discriminator-box", universalColor)
+        .setColor("message-box", universalColor)
+        .setColor("title", universalColor)
+        .setColor("avatar", universalColor)
+        .setBackground(imageLink)
+        .toAttachment()
+
+
+    let attachment = new Discord.MessageAttachment(image.toBuffer(), "leave-image.png");
+
+    member.guild.channels.cache.find(c => c.id === channel).send(attachment)
+})
 
 //Auto Pinging
 /*
