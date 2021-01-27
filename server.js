07cc6c30c0791737cefe0d1dc857d9ae48d37a9b
amasin76 <51626916+amasin76@users.noreply.github.com>
2021-01-27 20:04:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ client.on('guildMemberAdd', async member => {
         .setUsername(member.user.username)
         .setDiscriminator(member.user.discriminator)
         .setMemberCount(member.guild.memberCount)
-        .setGuildName(member.guild.name)
+        .setGuildName(/*member.guild.name*/"enable-pnpmThe Epics 💖")
         .setAvatar(member.user.displayAvatarURL({
             format: 'png'
         }))
@@ -69,7 +69,7 @@ client.on('guildMemberRemove', async member => {
         .setUsername(member.user.username)
         .setDiscriminator(member.user.discriminator)
         .setMemberCount(member.guild.memberCount)
-        .setGuildName(member.guild.name)
+        .setGuildName(/*member.guild.name*/"enable-pnpmThe Epics 💖")
         .setAvatar(member.user.displayAvatarURL({
             format: 'png'
         }))
