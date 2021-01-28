b679e76223c8efe9af38e13f3b21f246e4d3af4c
amasin76 <51626916+amasin76@users.noreply.github.com>
2021-01-28 23:12:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -97,6 +97,45 @@ client.on('message', async message => {
         client.emit('guildMemberRemove', message.member)
     }
 })
+//Welcome Msg========================
+const guildInvites = new Map();
+const {MessageEmbed} = require("discord.js")
+client.on("inviteCreate",async invite => guildInvites.set(invite.guild.id, await invite.guild.fetchInvites()));
+client.on("ready",() =>{
+    console.log(`${client.user.tag} is Online`)
+    client.guilds.cache.forEach(guild => {
+        guild.fetchInvites()
+        .then(invites => guildInvites.set(guild.id, invites))
+        .catch(err => console.log(err));
+
+
+    });
+});
+
+client.on("guildMemberAdd", async member => {
+    const cachedInvites = guildInvites.get(member.guild.id);
+    const newInvites = await member.guild.fetchInvites();
+    guildInvites.set(member.guild.id, newInvites);
+
+    try{
+        const usedInvite = newInvites.find(inv => cachedInvites.get(inv.code).uses < inv.uses);
+        const embed = new MessageEmbed()
+        //.setTitle(`Welcome to ${member.guild.name}`)
+        //.setDescription(`Hello ${member}, you are our ${member.guild.memberCount}th member\nJoined using ${usedInvite.inviter/*.username*/}'s link\nNumber of uses: ${usedInvite.uses}\nInvite Link: ${usedInvite.url}`)
+        .setDescription(`=✨**Welcome 💖${member}💖 in ${member.guild.name}**✨= \n ==✨**Invited by 💌${usedInvite.inviter/*.username*/}'s💌 Invite**✨==`)
+        .setColor("#8015EA")
+        //.setTimestamp()
+
+        const joinChannel = member.guild.channels.cache.find(channel => channel.id === "712769961004630171")
+        setTimeout(() => {
+        if(joinChannel) {
+            joinChannel.send(embed).catch(err => console.log(err))
+
+        }
+          }, 1500)
+    }
+    catch(err) {console.log(err);}
+})
 
 
 //Auto Pinging
