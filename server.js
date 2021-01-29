26b0f3cd2912a01838e849bff073909cc4fb6632
amasin76 <51626916+amasin76@users.noreply.github.com>
2021-01-29 14:49:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -122,8 +122,9 @@ client.on("guildMemberAdd", async member => {
         const embed = new MessageEmbed()
         //.setTitle(`Welcome to ${member.guild.name}`)
         //.setDescription(`Hello ${member}, you are our ${member.guild.memberCount}th member\nJoined using ${usedInvite.inviter/*.username*/}'s link\nNumber of uses: ${usedInvite.uses}\nInvite Link: ${usedInvite.url}`)
-        .setDescription(`=✨**Welcome 💖${member}💖 in ${member.guild.name}**✨= \n ==✨**Invited by 💌${usedInvite.inviter/*.username*/}'s💌 Invite**✨==`)
+        .setDescription(`=✨**Welcome 💖${member}💖 in ${member.guild.name}**✨= \n ==✨**Invited by 💌 __${usedInvite.inviter.tag/*.username*/}'s__💌 Invite** ✨==`)
         .setColor("#8015EA")
+        .setFooter(`Account age: 📆 ● ${moment(member.user.createdTimestamp).format('LL')} ● ${moment(member.user.createdTimestamp).fromNow()} ●`)
         //.setTimestamp()
 
         const joinChannel = member.guild.channels.cache.find(channel => channel.id === "717082044022390877")
@@ -132,7 +133,7 @@ client.on("guildMemberAdd", async member => {
             joinChannel.send(embed).catch(err => console.log(err))
 
         }
-          }, 1500)
+          }, 2000)
     }
     catch(err) {console.log(err);}
 })
