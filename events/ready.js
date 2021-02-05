32a1efa8bec57b58674bc68ab821c4059161eb9d
amasin76 <51626916+amasin76@users.noreply.github.com>
2021-02-05 06:15:08 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,17 +2,17 @@
 module.exports = async client => {
      console.log(`${client.user.username} im ready`)
     // bot.user.setActivity("Hello", {type: "STREAMING", url:"https://twitch.tv/Strandable"});
-
+    let userIn = await client.users.cache.size;
     let statuses = [
         //`${client.guilds.size} servers!`,
         "8help",
         "💖THE EPICS💖",
         "صلوا على شفيع الأمة",
-        `💖${client.users.cache.size} 𝙐𝙎𝙀𝙍𝙎💖`,
+        `💖${userIn} 𝙐𝙎𝙀𝙍𝙎💖`,
         "Javascript",
         "V1.1",
         "By | 7ANKALISS✨",
-        `💖${client.users.cache.size} 𝙐𝙎𝙀𝙍𝙎💖`,
+        `💖${userIn} 𝙐𝙎𝙀𝙍𝙎💖`,
     ]
 
     setInterval(function() {
