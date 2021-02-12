73001fb2c03600d92791afc9086ded8ad9fc5215
amasin76 <51626916+amasin76@users.noreply.github.com>
2021-02-12 10:35:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -148,6 +148,16 @@ client.on("guildMemberAdd", async member => {
 const jointocreate = require("./jointocreate");
 jointocreate(client);
 
+//Bot Mentioned=====================
+client.on('message', message=> {
+    if (message.author.bot) return;
+    if (message.author.id == process.env.BOT_OWNER) return message.channel.send("**سمعـــــا و طاعـــــة**");
+    if (message.mentions.has(client.user))
+    {
+    message.reply("** Ta Hnina o Hni lw9t **")
+    }
+});
+
 
 //Auto Pinging
 /*
